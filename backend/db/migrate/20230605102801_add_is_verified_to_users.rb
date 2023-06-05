43c3aae8c5a5cf4b0ff3d016f4bc28f40402e2af
dariberrie <darigoldman@gmail.com>
2023-06-05 13:14:29 +0200

    fix: db changes

--- backend/db/migrate/20230605102801_add_is_verified_to_users.rb
+++ backend/db/migrate/20230605102801_add_is_verified_to_users.rb
@@ -1,6 +1,5 @@
 class AddIsVerifiedToUsers < ActiveRecord::Migration[6.1]
   def change
-    remove_column :users, :isVerified
-    add_column :users, :isVerified, :boolean, default: false
+    
   end
 end
